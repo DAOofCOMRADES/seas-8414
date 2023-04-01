dc9af6a22842e74af37323d238d0c33bf671c02e
DAO of COMRADES <96131136+DAOofCOMRADES@users.noreply.github.com>
2023-04-01 07:25:49 -0400

    Delete week-6 directory

--- week-6/master-class/building-blocks/namespace/process-list.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import subprocess
-ps = subprocess.Popen("ps -U 0", shell=True, stdout=subprocess.PIPE)
-print ps.stdout.read()
-ps.stdout.close()
-ps.wait()
--- week-6/master-class/building-blocks/python/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    name = 'HelloWorld',
-    version = '0.1',
-    scripts = ['HelloWorld.py']
-)
