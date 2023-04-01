5e90fe16be93613f08de9a752510bb714e7a5552
DAO of COMRADES <96131136+DAOofCOMRADES@users.noreply.github.com>
2023-04-01 07:27:41 -0400

    Delete week-1/terraform directory

--- week-1/terraform/step-1/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
-}
-
-resource "aws_instance" "step1_server" {
-  ami           = "ami-08938d43921654967"
-  instance_type = "t2.nano"
-}
